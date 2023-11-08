45d7cca10961fb7657a7edecc20b5371592cb930
dageboyi <75166480+dageboyi@users.noreply.github.com>
2023-11-08 22:03:28 +1030

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors');
+
 const userRouter = require('./router/userRouter')
 const articleRouter = require('./router/articleRouter')
 const bodyparser = require('body-parser')
 const path = require('path')
 
 const app = express()
+app.use(cors());
 app.use(bodyparser.json())
 app.use('/', userRouter)
 app.use('/article', articleRouter)
